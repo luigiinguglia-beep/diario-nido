ba18f69d3ad1c25f98c2404f373f12ed8fcd2b10
luigiinguglia-beep <luigi.inguglia@gmail.com>
2026-08-07 11:50:20 +0200

    Update config.js

--- webapp/config.js
+++ webapp/config.js
@@ -1,4 +1,4 @@
 // Incolla qui i valori dal tuo progetto Supabase:
 // Dashboard -> Settings -> API
-window.SUPABASE_URL = "https://TUO-PROGETTO.supabase.co";
-window.SUPABASE_ANON_KEY = "INCOLLA_QUI_LA_ANON_KEY";
+window.SUPABASE_URL = "https://foxpetkzdahkuxztlgpd.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZveHBldGt6ZGFoa3V4enRsZ3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwOTAwMzIsImV4cCI6MjEwMTY2NjAzMn0.vlyrRTFq2bjwuGr4-LF7yVU9voyYqHuIy0oxtmzTt1E";
